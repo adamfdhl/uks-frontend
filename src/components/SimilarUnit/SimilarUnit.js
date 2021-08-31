520fbe17226629d1b9ceeaef4c8b9fc53bff456e
adamfdhl <adamfdhl@gmail.com>
2021-08-31 18:13:15 +0700

    more style on similar unit

--- src/components/SimilarUnit/SimilarUnit.js
+++ src/components/SimilarUnit/SimilarUnit.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { truncate } from "../../utils/helpers";
+import { truncate, getSimilarityColor } from "../../utils/helpers";
 
 import "./SimilarUnit.scss";
 
@@ -12,7 +12,24 @@ function SimilarUnit({ unit }) {
         <h4>{unit.judul_unit}</h4>
         <h2>{unit.kode_unit}</h2>
         <p>{truncate(unit.deskripsi_unit, 200)}</p>
-        <span>{`Tingkat kemiripan : ${unit.similarity_score * 100}%`}</span>
+        <div className="similarity_container">
+          <p className="DetailUnit__similarity">
+            Tingkat kemiripan :
+            <span
+              style={{
+                backgroundColor: getSimilarityColor(unit.similarity_score),
+                borderRadius: "5px",
+                padding: "5px 10px",
+                marginLeft: "5px",
+              }}
+            >
+              {unit.similarity_score * 100}%
+            </span>
+          </p>
+          <p className="DetailUnit__seemore">
+            Click untuk menuju detail unit lebih lanjut ->
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,15 @@
 export function truncate(sentence, maxLength) {
-  console.log(sentence.length);
   if (sentence.length > maxLength)
     return sentence.substring(0, maxLength) + "...";
   return sentence;
+}
+
+export function getSimilarityColor(similarityScore) {
+  if (similarityScore >= 0.65) {
+    return "#57f331";
+  } else if (similarityScore < 0.65 && similarityScore >= 0.5) {
+    return "rgb(252, 179, 96)";
+  } else {
+    return "rgb(243, 86, 59)";
+  }
 }
